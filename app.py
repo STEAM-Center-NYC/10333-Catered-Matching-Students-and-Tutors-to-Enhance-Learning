174131c99c103901d6dc14ac53c824cc4b6158da
AdrianC167 <ac232126110@gmail.com>
2024-03-13 10:09:22 -0400

    set up the landing page basics

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ from pprint import pprint as print
 app = Flask(__name__)
 
 @app.route("/", methods= ["GET", 'POST'])
-def landing():
+def home():
     
+    return render_template("index-page.html.jinja")
+
+
+@app.route("/land", methods= ["GET", 'POST'])
+def landing():
     return render_template("landing-page.html.jinja")
