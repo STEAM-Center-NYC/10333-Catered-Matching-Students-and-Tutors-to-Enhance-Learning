d8cbb45ade43424f269e970c3ea914c4d6853d83
AdrianC167 <ac232126110@gmail.com>
2024-03-20 10:43:01 -0400

    finished basics for signup

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask,g
+from flask import render_template,request, redirect
 import pymysql
 import pymysql.cursors
-from pprint import pprint as print
+import flask_login
+
 
 
 
@@ -17,6 +19,19 @@ def connect_db():
         autocommit=True
     )
 
+def get_db():
+    '''Opens a new database connection per request.'''        
+    if not hasattr(g, 'db'):
+        g.db = connect_db()
+    return g.db    
+
+@app.teardown_appcontext
+def close_db(error):
+    '''Closes the database connection at the end of request.'''    
+    if hasattr(g, 'db'):
+        g.db.close() 
+
+
 @app.route("/", methods= ["GET", 'POST'])
 def home():
     
@@ -30,4 +45,16 @@ def landing():
 
 @app.route("/signup-tutor", methods= ["GET", 'POST'])
 def signup_tutor():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        gender = request.form['gender']
+        education = request.form['education']
+        cursor = get_db().cursor()
+        cursor.execute(f"INSERT INTO `tutors`(`name` , `email`, `password`, `gender`,`education-level`) VALUES('{name}', '{email}', '{password}', '{gender}','{education}')")         
+        cursor.close()
+        get_db().commit()
+
+
     return render_template("signup-tutor.html.jinja")
