dd5d577368d90d792940aa81fce1b147b17cfd4f
Fahim Muntasir <famuntasir2@brooklynsteamcenter.org>
2024-03-21 10:35:23 -0400

    Done with sign up for students

--- app.py
+++ app.py
@@ -1,11 +1,35 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask,g
+from flask import render_template, request, redirect
 import pymysql
 import pymysql.cursors
-from pprint import pprint as print
+import flask_login
 
 
 
 app = Flask(__name__)
+
+def connect_db():
+    return pymysql.connect(
+    database = "tutoria",
+    user = "fmuntasir2",
+    password = "239442965",
+    host = "10.100.33.60",
+    cursorclass = pymysql.cursors.DictCursor,
+    autocommit=True
+    )
+
+
+def get_db():
+    '''Opens a new database connection per request.'''        
+    if not hasattr(g, 'db'):
+        g.db = connect_db()
+    return g.db    
+
+@app.teardown_appcontext
+def close_db(error):
+    '''Closes the database connection at the end of request.'''    
+    if hasattr(g, 'db'):
+        g.db.close() 
 
 @app.route("/", methods= ["GET", 'POST'])
 def home():
@@ -21,3 +45,22 @@ def landing():
 def contact():
     
     return render_template("contact-page.html.jinja")
+
+
+
+
+@app.route("/signup-students", methods= ["GET", 'POST'])
+def signup_student():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        gender = request.form['gender']
+        grade = request.form['grade']
+        cursor = get_db().cursor()
+        cursor.execute(f"INSERT INTO `students`(`student-name` , `student-email`, `password`, `gender`, `grade-level`) VALUES('{name}', '{email}', '{password}', '{gender}', '{grade}')")         
+        cursor.close()
+        get_db().commit()
+
+
+    return render_template("signup-students.html.jinja")
