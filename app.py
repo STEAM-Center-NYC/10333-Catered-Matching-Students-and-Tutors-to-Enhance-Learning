f94988aeedc5b8d86cfb9435fe0001b5b1b8aa41
Jedi830 <jdelacruz@brooklynsteamcenter.org>
2024-03-22 10:09:57 -0400

    Sign In page

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 from flask import Flask, render_template, request, redirect
+from dynaconf import Dynaconf
 import pymysql
 import pymysql.cursors
 from pprint import pprint as print
 
 
+settings = Dynaconf(
+    settings_file = ['settings.toml']
+)
 
 app = Flask(__name__)
+
+def connect_db():
+    return pymysql.connect(
+        host = "10.100.33.60",
+        user = settings.db_user,
+        password = settings.db_pass,
+        database = settings.db_name,
+        cursorclass = pymysql.cursors.DictCursor,
+        autocommit = True
+    )
+
 
 @app.route("/", methods= ["GET", 'POST'])
 def home():
@@ -15,4 +30,10 @@ def home():
 
 @app.route("/land", methods= ["GET", 'POST'])
 def landing():
+
     return render_template("landing-page.html.jinja")
+
+@app.route("/signin", methods= ["GET", 'POST'])
+def SignIn():
+
+    return render_template("sign-in-page.html.jinja")
