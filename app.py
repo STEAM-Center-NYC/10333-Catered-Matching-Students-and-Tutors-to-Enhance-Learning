990aa559ec4ed19d3f18467c1dd64ed9a1e30867
Jaybksteam <jedouard@brooklynsteamcenter.org>
2024-04-18 08:59:48 -0400

    Change

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, g
+from flask import Flask, render_template, request, redirect, g, flash,url_for
 import pymysql
 import pymysql.cursors
 from dynaconf import Dynaconf
@@ -9,26 +9,70 @@ from wtforms import FileField,SubmitField
 from werkzeug.utils import secure_filename
 import os
 
+
+
+
 settings = Dynaconf(
     settings_file =  ['settings.toml']
 )
 
 
-
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'jhbgdifjhujne3hr3@#J$JERri32@%j4#FLD?SRJF#ORJ$D>R>>$%K$GRIFJTi4OJrhjedfdsojfrSHTIREJHTOJRSJLFN568785345=--213;'
+app.config['UPLOAD_FOLDER'] = 'media'
+
+
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+
+class User:
+    is_authenticated = True
+    is_auonymous = False
+    is_active = True
+
+    def __init__(self, id, email, dob, gender , subject, educational_level, role, name): 
+        
+        self.id = id
+        self.email = email
+        self.dob = dob
+        self.gender = gender
+        self.subject = subject
+        self.educational_level = educational_level
+        self.role = role
+        self.name = name
+
+    def get_id(self):
+        return str(self.id)
+        
+
+@login_manager.user_loader
+def load_user(user_id):
+    cursor = get_db().cursor()
+    cursor.execute(f"SELECT * FROM `users` WHERE `id` = {user_id} ")
+    result = cursor.fetchone()
+    cursor.close
+    get_db().commit()
+
+    if result is None:
+        return None
+    
+    return User(result["id"], result["email"],result['dob'],result['gender'],result['subject'],result['educational_level'],result['role'],result['name'])
 
 
 
 
+class UploadFileForm(FlaskForm):
+    file = FileField("File")
+    submit = SubmitField("Upload File")
 
 def connect_db():
     return pymysql.connect(
-        host="10.100.33.60",
-        user= settings.db_user,
-        password= settings.db_pass,
-        database= settings.db_name,
-        cursorclass=pymysql.cursors.DictCursor,
-        autocommit=True
+        host = "10.100.33.60",
+        user = settings.db_user,
+        password = settings.db_pass,
+        database = settings.db_name,
+        cursorclass = pymysql.cursors.DictCursor,
+        autocommit = True
     )
 
 
@@ -36,7 +80,7 @@ def get_db():
     '''Opens a new database connection per request.'''        
     if not hasattr(g, 'db'):
         g.db = connect_db()
-    return g.db    
+    return g.db   
 
 @app.teardown_appcontext
 def close_db(error):
@@ -44,38 +88,22 @@ def close_db(error):
     if hasattr(g, 'db'):
         g.db.close() 
 
-@app.route("/index", methods= ["GET", 'POST'])
+@app.route("/home", methods= ["GET", 'POST'])
+@flask_login.login_required
 def home():
-    cursor = get_db().cursor()
-    cursor.execute('SELECT * FROM `users` ')
-    result = cursor.fetchall()
-    cursor.close()
-    return render_template("index-page.html.jinja", result =result)
-
-
-@app.route("/land", methods= ["GET", 'POST'])
-def landing():
     
+    return render_template("index-page.html.jinja")
+
+
+@app.route("/", methods= ["GET", 'POST'])
+def landing():
 
     return render_template("landing-page.html.jinja")
 
-@app.route("/contact", methods= ["GET", 'POST'])
-def contact():
-    
-    return render_template("contact-page.html.jinja")
-
-
-    if password == result["password"]:
-            user = load_user(result['id'])
-            flask_login.login_user(user)
-            return redirect('/')
-    if flask_login.current_user.is_authenticated:
-        return redirect("/")
-    return render_template("signin-page.html.jinja")
 
 
 @app.route('/signin', methods = ['GET','POST'])
-def signin():
+def sign_in():
     if request.method == 'POST':
         email = request.form["email"]
         password = request.form["password"]
@@ -90,9 +118,8 @@ def signin():
             flask_login.login_user(user)
             return redirect('/')
     if flask_login.current_user.is_authenticated:
-        return redirect("/")
+        return redirect("/home")
     return render_template("signin-page.html.jinja")
-
 
 
 
@@ -147,9 +174,14 @@ def profile():
         cursor = get_db().cursor()
         cursor.execute(f"UPDATE users SET profile_img = '{file_name}' WHERE id = {user.id}")             
         cursor.close()
-        return ("File has been uploaded.")  
-    return render_template("profile.html.jinja", form=form)
-
+        flash("File has been uploaded.")
+        return redirect(url_for("profile"))
+    user = flask_login.current_user
+    cursor = get_db().cursor()
+    cursor.execute(f'SELECT * FROM `users` WHERE `id` = "{user.id}"')
+    result = cursor.fetchone()
+    cursor.close()
+    return render_template("profile.html.jinja", form=form, result = result)
 
 
 
